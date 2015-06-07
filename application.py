84ebee1a350b3b434ef4432368ee1c69d3a9b2be
udayj <jhunjhunwala.uday@gmail.com>
2015-06-07 12:30:23 +0530

    returning normal string response from voice_call function

--- application.py
+++ application.py
@@ -830,8 +830,8 @@ def voice_call():
 	        r.number(dest_number)
 	    else:
 	        r.client(default_client)
-	response=Response(resp,status=200,mimetype='application/xml')
-	return response
+
+	return str(resp)
 
 def task_worker(_id,instant=False):
 	def send_mail(data,files):
